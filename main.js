4cb0d781804ccd6a08afaca8f6a7c29d93f0bbec
Mohammed Fakih <mhd.fakih@gmail.com>
2023-12-09 01:07:37 +0300

    javascript part done

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-'use strict'
+"use strict";
+
+const viewsEl = document.getElementById("views");
+const priceEl = document.getElementById("price");
+const slider = document.getElementById("range");
+const checkbox = document.getElementById("checkbox");
+const monthlyPrices = [8, 12, 16, 24, 36];
+const viewsArr = [
+  "10K pageviews",
+  "50K pageviews",
+  "100K pageviews",
+  "500k pageviews",
+  "1M pageviews",
+];
+const yearlyDiscount = 0.25;
+
+slider.addEventListener("input", (event) => calculatePrice(event));
+
+const calculatePrice = (event) => {
+  viewsEl.textContent = viewsArr[event.target.value];
+  if (!checkbox.checked) {
+    priceEl.textContent = "$" + monthlyPrices[event.target.value];
+  } else {
+    let amount = Math.abs(
+      monthlyPrices[event.target.value] * 12 * 0.25 -
+        monthlyPrices[event.target.value] * 12
+    );
+    priceEl.textContent = "$" + amount;
+  }
+};
+checkbox.addEventListener("change", togglePriceCalculte);
+
+function togglePriceCalculte() {
+  viewsEl.textContent = viewsArr[slider.value];
+  if (!checkbox.checked) {
+    priceEl.textContent = "$" + monthlyPrices[slider.value];
+  } else {
+    let amount = Math.abs(
+      monthlyPrices[slider.value] * 12 * 0.25 - monthlyPrices[slider.value] * 12
+    );
+    priceEl.textContent = "$" + amount;
+  }
+}
